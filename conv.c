528560499667a44e2b91a9009934c53a525fece5
MissTrap <60643253+MissTrap@users.noreply.github.com>
2022-06-06 19:39:33 +0200

    Add files via upload

---
+++ conv.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <assert.h>
+
+#define SERVER "lower"
+#define MAX 512
+#define TEST "This is a TeSt to SEE if iT wORks"
+#define CLIENT "help"
+
+int main(void) {
+  int sock;
+  char buffer[MAX];
+
+  struct sockaddr_un name = {AF_UNIX, CLIENT};
+
+  struct sockaddr_un server = {AF_UNIX, SERVER};
+  int size = sizeof(struct sockaddr_un);
+
+  int n = sizeof(TEST);
+
+  assert(sendto(sock, TEST, n, 0, (struct sockaddr *) &server, size) != -1);
+  n = recvfrom(sock, buffer, MAX-1, 0, (struct sockaddr *) &server, &size);
+  if (n == -1) {
+    perror("server");
+  }
+
+  buffer[n] = 0;
+  printf("Client received %s\n", buffer);
+  unlink(CLIENT);
+  exit(0);
+}
---
+++ hello.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <assert.h>
+
+#define MAX 512
+#define TEST "Hello, hello"
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 8080
+
+int main(void) {
+  int sock;
+  char buffer[MAX];
+
+  /* A socket is created */
+  assert((sock = socket(AF_INET, SOCK_DGRAM,0)) != -1);
+
+  struct sockaddr_in name;
+
+  name.sin_family = AF_INET;
+  name.sin_port = 0;
+  name.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  assert(bind(sock, (struct sockaddr *) &name, sizeof(name)) != -1);
+
+  struct sockaddr_in server;
+
+  server.sin_family = AF_INET;
+  server.sin_port = htons(SERVER_PORT);
+  server.sin_addr.s_addr = inet_addr(SERVER_IP);
+}
